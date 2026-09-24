59fa1d9f4607ab51d814005836d96d1e0c1f19f4
abovebeyond4north-netizen <above.beyond.4north@gmail.com>
2026-09-24 19:31:11 -0400

    ADI Genesis-15: expose architecture contract

--- adi_genesis15/__init__.py
+++ adi_genesis15/__init__.py
@@ -1,1 +1,15 @@
 """ADI Genesis-15 uncertainty-gated partially observed world-model experiment."""
+
+from .contract import (
+    ArchitectureSnapshot,
+    ArchitectureViolation,
+    assert_genesis15_architecture,
+    snapshot_from_payloads,
+)
+
+__all__ = [
+    "ArchitectureSnapshot",
+    "ArchitectureViolation",
+    "assert_genesis15_architecture",
+    "snapshot_from_payloads",
+]
