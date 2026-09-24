08e956365df925d26b1cefe8f4c85b823a501863
abovebeyond4north-netizen <above.beyond.4north@gmail.com>
2026-09-24 19:47:48 -0400

    ADI Genesis-16: expose architecture contract

--- adi_genesis16/__init__.py
+++ adi_genesis16/__init__.py
@@ -1,1 +1,15 @@
 """ADI Genesis-16 nonlinear uncertainty experiment."""
+
+from .contract import (
+    ArchitectureSnapshot,
+    ArchitectureViolation,
+    assert_genesis16_architecture,
+    snapshot_from_payloads,
+)
+
+__all__ = [
+    "ArchitectureSnapshot",
+    "ArchitectureViolation",
+    "assert_genesis16_architecture",
+    "snapshot_from_payloads",
+]
